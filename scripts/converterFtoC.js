43ebf869bab4a3571282814a59c665062a1ba277
Alcalde Gomez Leonardo Esteban <leonardoalcalde020@gmail.com>
2024-09-03 19:11:42 -0300

    Fahrenheit to Celcius converter added and tested

---
+++ scripts/converterFtoC.js
@@ -0,0 +1,1 @@
+export const converterFtoC = (FahrenheitTemp) => 5 / 9 * (FahrenheitTemp - 32)
--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+import { converterFtoC } from './converterFtoC.js'
 import { getWeatherData } from './getWeatherData.js'
 import { renderer } from './renderer.js'
 
@@ -6,3 +7,4 @@ renderer()
 const weatherData = await getWeatherData('Chilecito')
 
 console.log(weatherData)
+console.log(converterFtoC(65))
