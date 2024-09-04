44c7fbdea0462cc33299fecff004df0130c0b06a
Alcalde Gomez Leonardo Esteban <leonardoalcalde020@gmail.com>
2024-09-04 19:00:24 -0300

    Implementation of todayCard started
    - fix somethings css styles and todayCard's style added
    - todayCard started and first tests done

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,10 @@
 import { formCard } from './formCard.js'
+import { todayCard } from './todayCard.js'
 import { renderer } from './renderer.js'
+import { getWeatherData } from './getWeatherData.js'
+
+const weatherData = await getWeatherData('chilecito')
+console.log(weatherData.currentConditions)
 
 renderer(formCard())
+renderer(todayCard(weatherData))
