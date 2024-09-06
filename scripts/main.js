657b08845b3c65c9082637a536c84af50721b814
Alcalde Gomez Leonardo Esteban <leonardoalcalde020@gmail.com>
2024-09-06 20:33:25 -0300

    unnesesary line deleted

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,6 @@
 import { formCard } from './formCard.js'
 import { renderer } from './renderer.js'
-import { getWeatherData } from './getWeatherData.js'
 import { parallaxFX } from './parallaxFx.js'
-
-const weatherData = await getWeatherData('chilecito')
 
 renderer(formCard())
 parallaxFX()
