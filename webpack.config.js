ecdb8a87a6dff5b81898ca23505019256f42e4bf
Alcalde Gomez Leonardo Esteban <leonardoalcalde020@gmail.com>
2024-09-07 14:45:47 -0300

    styleless page on webpack started

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: './src/scripts/main.js',
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    path: path.resolve(__dirname, './dist'),
   },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
 };
