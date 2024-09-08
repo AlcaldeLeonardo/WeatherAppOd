d815577c6be8a0d660042636c2d256f103a4f1df
Alcalde Gomez Leonardo Esteban <leonardoalcalde020@gmail.com>
2024-09-08 15:22:36 -0300

    optimized background

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,40 @@
-const ESLintWebpackPlugin = require("eslint-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const ESLintWebpackPlugin = require('eslint-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
 
 module.exports = {
-  entry: "./src/scripts/main.js",
+  entry: './src/scripts/main.js',
   output: {
-    filename: "index_bundle.js",
-    path: path.resolve(__dirname, "./dist"),
-    clean: true,
+    filename: 'index_bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    clean: true
   },
+  mode: 'production',
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: "img/[name][ext]",
+          filename: 'img/[name][ext]'
         }
-      },
-    ],
+      }
+    ]
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
-      favicon: "./src/img/weatherAppIcon.png",
+      template: './src/index.html',
+      favicon: './src/img/weatherAppIcon.png'
     }),
     new ESLintWebpackPlugin({
-      extensions: ["js"],
+      extensions: ['js'],
       fix: true,
       failOnError: true,
-      overrideConfigFile: path.resolve(__dirname, ".eslintrc.json"),
+      overrideConfigFile: path.resolve(__dirname, '.eslintrc.json')
     })
-  ],
-};
+  ]
+}
